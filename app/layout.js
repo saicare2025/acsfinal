049b353390634f9f7d0e6e8d136d6aad1fdc4304
Elisa Lawyer <elisalawyer@gmail.com>
2025-08-19 19:36:30 +0600

    updated

--- app/layout.js
+++ app/layout.js
@@ -55,55 +55,7 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
-        <Script
-          id="tiktok-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function (w, d, t) {
-                w.TiktokAnalyticsObject=t;
-                var ttq=w[t]=w[t]||[];
-                ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
-                ttq.setAndDefer=function(t,e){
-                  t[e]=function(){
-                    t.push([e].concat(Array.prototype.slice.call(arguments,0)));
-                  };
-                };
-                for(var i=0;i<ttq.methods.length;i++){
-                  ttq.setAndDefer(ttq,ttq.methods[i]);
-                }
-                ttq.instance=function(t){
-                  var e=ttq._i[t]||[];
-                  for(var n=0;n<ttq.methods.length;n++){
-                    ttq.setAndDefer(e,ttq.methods[n]);
-                  }
-                  return e;
-                };
-                ttq.load=function(e,n){
-                  var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;
-                  ttq._i=ttq._i||{};
-                  ttq._i[e]=[];
-                  ttq._i[e]._u=r;
-                  ttq._t=ttq._t||{};
-                  ttq._t[e]=+new Date;
-                  ttq._o=ttq._o||{};
-                  ttq._o[e]=n||{};
-                  n=document.createElement("script");
-                  n.type="text/javascript";
-                  n.async=!0;
-                  n.src=r+"?sdkid="+e+"&lib="+t;
-                  e=document.getElementsByTagName("script")[0];
-                  e.parentNode.insertBefore(n,e);
-                };
-              
-                ttq.load('C5B6LNC6J7TSRVQBTN00');
-                ttq.page();
-              }(window, document, 'ttq');
-            `,
-          }}
-        />
-
-        {/* Provesrc - Load lazily to reduce initial blocking */}
+       
         {/* <Script id="provesrc" strategy="lazyOnload">
           {`
             if (!window.provesrc) {
