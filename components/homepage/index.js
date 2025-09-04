f9f3046b3f454beda1503cc03e0e0748317a4c01
Seb Master <elisalawyer@gmail.com>
2025-09-04 15:50:41 +0600

    Update index.js

--- components/homepage/index.js
+++ components/homepage/index.js
@@ -8,19 +8,14 @@ import CTASection from "./CTA";
 import HomeClientSections from "./HomeClientSections";
 import CreditRepairSection from "./CreditRepair";
 import CreditRepairProcess from "./CreditRepairProcess.js";
-import ProductReviewWidget from "./TestmonialProduct"
+import Testimonials from "./Testmonials"
 
 export default function HomePage() {
   return (
     <main className="min-h-screen">
       <HeroSection />
       <TrustBadge />
-      <ProductReviewWidget
-        // brandId can be overridden if needed:
-        // brandId="407543ca-1287-43ab-a66b-5f78dfd94a6a"
-        className="rounded-2xl border p-4"
-        placeholder="Fetching verified reviews…"
-      />
+      <Testimonials />
       <WhyChooseUs />
       <HelpSection />
       <CompanyCarousel />
