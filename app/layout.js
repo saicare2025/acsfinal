c501de1155b98cf7fe7bd85bd5419505006365b8
Seb Master <elisalawyer@gmail.com>
2025-09-08 13:42:53 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -31,7 +31,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Preconnects to reduce connection latency */}
+    
         <link
           rel="preconnect"
           href="https://www.googletagmanager.com"
